702e1c93570c0ed35ff2afe2889fd4faa086284c
Kevin Ji <1146876+kevinji@users.noreply.github.com>
2023-01-18 10:25:32 -0800

    Rename package to solana-forwarder

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 use solana_client::rpc_client::RpcClient;
 use std::env;
-
-
 
 #[get("/healthCheck")]
 async fn health_check() -> String {
     let solana_endpoint = env::var("SOLANA_ENDPOINT").expect("SOLANA_ENDPOINT not set");
     let rpc: RpcClient = RpcClient::new(solana_endpoint);
     let response = rpc.get_health();
-    
+
     match response {
         Ok(_) => String::from("Health Ok!"),
         Err(e) => format!("Error: {:?}", e),
     }
 }
-
 
 #[launch]
 #[tokio::main]
